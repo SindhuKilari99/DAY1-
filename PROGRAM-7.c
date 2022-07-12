147ddfa70965595dbfc888d8a71e25f762cf5ab9
SindhuKilari99 <108974193+SindhuKilari99@users.noreply.github.com>
2022-07-12 21:53:57 +0530

    Create PROGRAM-7.c

---
+++ PROGRAM-7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num,rem,sum=0,n;
+    printf("enter the number ");
+    scanf("%d",&num);
+    if(n>0)
+    {
+        num=n%10;
+        sum=sum+rem;
+        num=n/10;
+    printf("palindrome",sum);
+    }
+    else
+    printf("not palindrome",sum);
+}
